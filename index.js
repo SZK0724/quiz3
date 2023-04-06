38c049eefec4e51c18cad6390a59a973647e967e
SAM ZHI KANG <b022120028@student.utem.edu.my>
2023-04-06 15:41:13 +0800

    client and server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send('Login')
+  })
+
+app.get('/', (req, res) => {
+  res.send('halloooooooooooooooooo!')
+})
+
+app.get('/bye', (req, res) => {
+    res.send('bye bye')
+  })
+
+
+  app.post('/register', (req, res) => {
+    res.send('acc created!')
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
